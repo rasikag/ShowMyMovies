dc0d2dc1aee435f89ab52d4d7487e6dcbe1a8e0a
Rasika Gayan Gunarathna <rasikagayan90@gmail.com>
2018-05-22 00:52:54 +0800

    added movie entity

---
+++ ShowMyMovies/Models/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShowMyMovies.Models
+{
+    [Table("Movies")]
+    public class Movie
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}
