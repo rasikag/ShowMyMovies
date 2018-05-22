3a90bd46725ca4febe70c26c70b407a77b1b4913
Rasika Gayan Gunarathna <rasikagayan90@gmail.com>
2018-05-23 01:03:18 +0800

    update the model class

--- ShowMyMovies/Models/Movie.cs
+++ ShowMyMovies/Models/Movie.cs
@@ -13,6 +13,8 @@ namespace ShowMyMovies.Models
         [Key]
         public int ID { get; set; }
         public string Title { get; set; }
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
         public decimal Price { get; set; }
