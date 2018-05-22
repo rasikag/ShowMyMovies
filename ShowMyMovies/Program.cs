8da1f72c638997ade2efbbe2c92db143064a8e35
Rasika Gayan Gunarathna <rasikagayan90@gmail.com>
2018-05-23 00:08:30 +0800

    seed initializer

--- ShowMyMovies/Program.cs
+++ ShowMyMovies/Program.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using System;
+using Microsoft.Extensions.DependencyInjection;
+using ShowMyMovies.Models;
 
 namespace ShowMyMovies
 {
@@ -14,7 +17,25 @@ namespace ShowMyMovies
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            var host = BuildWebHost(args);
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    // Requires using MvcMovie.Models;
+                    SeedData.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
